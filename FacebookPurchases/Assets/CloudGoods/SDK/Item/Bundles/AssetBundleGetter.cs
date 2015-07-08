07b88f3b6d6b599e9323ae0dcc7df302a6c244a6
Lionel Sy <lionely.sy@gmail.com>
2015-07-08 14:47:25 -0400

    Fixed more margining issues in bundle store and item store

---
+++ FacebookPurchases/Assets/CloudGoods/SDK/Item/Bundles/AssetBundleGetter.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="AssetBundleGetter.cs" company="SocialPlay">
+//     Copyright statement. All right reserved
+// </copyright>
+// Owner: Alex Zanfir & Mike Oliver
+// Date: 11/15/2012
+// Description: This is a utility class for downloading asset bundles
+// ------------------------------------------------------------------------
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using CloudGoods.Services;
+
+namespace SocialPlay.Bundles
+{
+    internal class AssetBundleGetter
+    {
+        public static void LoadFromCacheOrDownload(string assetName, int bundleVersion, Action<string, AssetBundle> callback, bool isAlwaysCallback=false)
+        {
+            string assetURL = CloudGoodsSettings.BundlesUrl +assetName + ".unity3d";
+
+            int version = bundleVersion;
+
+            BundlePacket.Create(assetURL, bundle =>
+            {
+                callback(assetName, bundle);
+            }, version,isAlwaysCallback);
+        }
+
+    }
+}
---
+++ FacebookPurchases/Assets/CloudGoods/SDK/Models/Response/StandardCurrencyBalanceResponse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using CloudGoods.SDK.Models;
+
+namespace CloudGoods.SDK.Models
+{
+    public class StandardCurrencyBalanceResponse
+    {
+        public int Total;
+        public List<SimpleItemInfo> Items;
+    }
+}
